ad1904e89a3cb7a889c300719e558b647bcb239e
Raymond516 <raymondgunawan025@gmail.com>
2026-08-10 16:32:32 +0700

    complete

--- Includes/footer.php
+++ Includes/footer.php
@@ -4,7 +4,6 @@
         <p class="text-muted mb-0">&copy; <?php echo date("Y"); ?> Toko Promosi B2B. Semua Hak Cipta Dilindungi.</p>
     </footer>
 
-    <!-- Memanggil Javascript Bootstrap 5 -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="interactive.js" defer></script>
 
